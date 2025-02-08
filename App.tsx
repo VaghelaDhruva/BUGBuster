a59059e71ade3f0d45adc800ccebc3b0b6ae1172
VaghelaDhruva <dhruvavaghela302006@gmail.com>
2025-02-08 14:01:55 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { AuthProvider } from "./hooks/use-auth";
+import NotFound from "@/pages/not-found";
+import AuthPage from "@/pages/auth-page";
+import HomePage from "@/pages/home-page";
+import QuizPage from "@/pages/quiz-page";
+import LeaderboardPage from "@/pages/leaderboard-page";
+import { ProtectedRoute } from "./lib/protected-route";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/auth" component={AuthPage} />
+      <ProtectedRoute path="/" component={HomePage} />
+      <ProtectedRoute path="/quiz" component={QuizPage} />
+      <ProtectedRoute path="/leaderboard" component={LeaderboardPage} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
